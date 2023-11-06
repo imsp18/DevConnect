b8a5d926599a321f41c348dca273c8b252eb2b98
Shivang Parmar <shivangparmar537@gmail.com>
2023-11-06 23:08:17 +0530

    added auth0 to the project enabling user to login logout using various socials

---
+++ src/components/LoginButton.js
@@ -0,0 +1,15 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+    return (
+        !isAuthenticated && (
+            <button onClick={()=> loginWithRedirect()} className="rounded-full border-white border flex flex-row items-center justify-center px-10 py-2 max-sm:px-4 max-sm:py-1 hover:cursor-pointer">
+                <h1 className="font-Poppins font-medium text-2xl bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent relative max-sm:text-lg">Login</h1>
+            </button>
+        )
+
+    )
+}
+
+export default LoginButton;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,7 @@
 import logo from '../assets/logo.png';
 import '../App.css';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 const Navbar = () => {
     return (
         <nav className="flex flex-row items-center justify-between mt-9 max-sm:mt-4">
@@ -8,10 +10,9 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" className="w-[46px] h-[51px] max-sm:w-[23px] max-sm:h-[25px]" />
                 <h1 className="font-Poppins font-bold text-3xl text-white ml-2 max-sm:text-lg">DevConnect</h1>
             </div>
-            {/* Login Button */}
-            <div class="rounded-full border-white border flex flex-row items-center justify-center px-10 py-2 max-sm:px-4 max-sm:py-1 hover:cursor-pointer">
-                <h1 className="font-Poppins font-medium text-white text-2xl bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent relative max-sm:text-lg">Login</h1>
-            </div>
+            {/* Button */}
+            <LoginButton />
+            <LogoutButton />
         </nav>
     );
 }
